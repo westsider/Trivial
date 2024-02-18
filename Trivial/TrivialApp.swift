dea5f9e1d114941802bee1c026346106024c2bc2
westsider <whansen1@mac.com>
2024-02-18 06:59:10 -0600

    Initial Commit

---
+++ Trivial/TrivialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivialApp.swift
+//  Trivial
+//
+//  Created by Warren Hansen on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TrivialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
